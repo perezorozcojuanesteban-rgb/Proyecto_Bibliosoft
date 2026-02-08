ef09d9114d4f024aa05321cf00994208f54beb44
Juan <perezorozcojuanesteban@gmail.com>
2026-02-08 17:26:51 -0500

    Se crea repositorio IAutor con JpaRepository y método de búsqueda por nombre

--- src/main/java/com/Bibliosoft/Bibliosoft/Repositorio/IAutor.java
+++ src/main/java/com/Bibliosoft/Bibliosoft/Repositorio/IAutor.java
@@ -1,4 +1,12 @@
 package com.Bibliosoft.Bibliosoft.Repositorio;
 
-public interface IAutor {
+import com.Bibliosoft.Bibliosoft.Modelo.MAutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IAutor extends JpaRepository<MAutor,Integer> {
+    List<MAutor>findByNombreautor(String Nombreautor);
 }
