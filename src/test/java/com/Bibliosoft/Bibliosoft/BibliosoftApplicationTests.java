89487539076d40fe300d84c6427afe640ad1cf5c
Juan <perezorozcojuanesteban@gmail.com>
2026-02-08 12:33:36 -0500

    Se crea encarpetado con spring boot para dar comienzo al proyecto Bibliosoft

---
+++ src/test/java/com/Bibliosoft/Bibliosoft/BibliosoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bibliosoft.Bibliosoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliosoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
